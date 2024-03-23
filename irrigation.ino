8d5c5b9c8d944c2995140005efb4a64d18f04cea
dhanu2711 <164607367+dhanu2711@users.noreply.github.com>
2024-03-23 20:28:35 +0530

    Add files via upload

---
+++ irrigation.ino
@@ -0,0 +1,50 @@
+#include <DHT.h>
+#define MOISTURE_SENSOR_PIN A0
+#define DHTPIN 2
+#define DHTTYPE DHT11
+#define MOTOR_PIN 5
+#define THRESHOLD_MOISTURE 1000
+#define THRESHOLD_TEMPERATURE 35
+
+DHT dht(DHTPIN, DHTTYPE);
+void setup()
+{
+   pinMode(MOISTURE_SENSOR_PIN, INPUT);
+  Serial.begin(9600);
+  dht.begin();
+  pinMode(MOTOR_PIN, OUTPUT);
+}
+
+void loop()
+{
+  delay(2000); 
+  int moistureLevel = analogRead(MOISTURE_SENSOR_PIN);
+  float temperature = dht.readTemperature();
+  float h = dht.readHumidity();
+
+  if (isnan(temperature)) 
+  {
+    Serial.println("Failed to read from DHT sensor!");
+    return;
+  }
+   Serial.print("Moisture Level: ");
+  Serial.println(moistureLevel);
+  Serial.print("Temperature: ");
+  Serial.println(temperature);
+   Serial.print("Humidity: ");
+   Serial.print(h);
+  
+
+  
+  if (moistureLevel > THRESHOLD_MOISTURE && temperature > THRESHOLD_TEMPERATURE)
+  {
+    digitalWrite(MOTOR_PIN, HIGH);
+    Serial.println("\nMOISTURE above threshold! .");
+  } 
+  else 
+  {
+
+    digitalWrite(MOTOR_PIN, LOW);
+    Serial.println("\nMOISTURE below threshold. .");
+  }
+}
