19d4c0465dc2fa879d7793c5ca9fd0fde57438ac
Sania823 <120888021+Sania823@users.noreply.github.com>
2024-03-24 09:50:36 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,44 @@
+from fastapi import FastAPI, File, UploadFile
+import uvicorn
+import numpy as np
+from io import BytesIO
+from PIL import Image
+import tensorflow as tf
+
+
+app = FastAPI()
+
+MODEL = tf.keras.models.load_model("models\ model.keras")
+try:
+    MODEL = tf.keras.models.load_model(r"models\ model.keras")
+except Exception as e:
+    print("Error loading model:", e)
+CLASS_NAMES = ['Tomato_Bacterial_spot',
+ 'Tomato_Early_blight',
+ 'Tomato_Late_blight',
+ 'Tomato_Leaf_Mold',
+ 'Tomato_Septoria_leaf_spot',
+ 'Tomato_healthy']
+ 
+
+
+def read_file_as_image(data)->np.ndarray:
+    image = np.array(Image.open(BytesIO(data)))
+    return image
+
+@app.post("/predict")
+async def predict(
+    file: UploadFile = File()):
+    image = read_file_as_image(await file.read())
+    img_batch = np.expand_dims(image,0)
+    prediction = MODEL.predict(img_batch)
+    predicted_class = CLASS_NAMES[np.argmax(prediction[0])]
+    confidence = np.max(prediction[0])
+    return {
+        'class': predicted_class,
+        'confidence': float(confidence)
+    }
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host = 'localhost', port = 8000)
